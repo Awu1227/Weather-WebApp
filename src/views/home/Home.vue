5043c3a9059d5ebb5adc80d74a6ef0e045e2c98c
dingxuewu <2497436726@qq.com>
2021-08-31 20:49:54 +0800

    -

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-  <router-link to="/search"
-    ><div class="top-2  w-40 bg-green-300">搜索页</div></router-link
-  >
-  <router-link to="/changeskin"
-    ><div class="top-2  w-40 bg-yellow-200">换肤页</div></router-link
-  >
-  <router-link to="/setting"
-    ><div class="top-2  w-40 bg-purple-300">设置页</div></router-link
-  >
-  <div>我是首页</div>
-  <button @click="handleClick">Fetch</button>
-  <realtime/>
-  <h1>我是首页，当前城市为{{this.$store.state.city}}，location为{{this.$store.state.location}}</h1>
-</div>
+    <router-link to="/search"
+      ><div class="top-2  w-40 bg-green-300">搜索页</div></router-link
+    >
+    <router-link to="/changeskin"
+      ><div class="top-2  w-40 bg-yellow-200">换肤页</div></router-link
+    >
+    <router-link to="/setting"
+      ><div class="top-2  w-40 bg-purple-300">设置页</div></router-link
+    >
+    <div>我是首页</div>
+    <button @click="handleClick">Fetch</button>
+    <realtime />
+    <h1>
+      我是首页，当前城市为{{ this.$store.state.city }}，location为{{
+        this.$store.state.location
+      }}
+    </h1>
+  </div>
 </template>
 
 <script>
