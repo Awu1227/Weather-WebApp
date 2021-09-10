0d53e5ce10dec39a1da4fa2e6075ef6c90da462f
dingxuewu <2497436726@qq.com>
2021-09-10 18:42:24 +0800

    feat(components): 首页顶部

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,7 @@
 <template>
-  <div>
-    <router-link to="/search"
-      ><div class="top-2  w-40 bg-green-300">搜索页</div></router-link
-    >
-    <router-link to="/changeskin"
-      ><div class="top-2  w-40 bg-yellow-200">换肤页</div></router-link
-    >
-    <router-link to="/setting"
-      ><div class="top-2  w-40 bg-purple-300">设置页</div></router-link
-    >
-    <div>我是首页</div>
+  <div id="main">
+    <home-top />
+    <now-weather />
     <button @click="handleClick">Fetch</button>
     <h1>
       我是首页，当前城市为{{ this.$store.state.city }}，location为{{
@@ -17,14 +9,18 @@
       }}
     </h1>
     <realtime />
+    ,,,,,,,,,
   </div>
 </template>
 
 <script>
 import Get from 'utils/request.js'
 import Realtime from './Realtime.vue'
+import HomeTop from './components/Home-top.vue'
+import NowWeather from './components/Now-weather.vue'
+
 export default {
-  components: { Realtime },
+  components: { Realtime, HomeTop, NowWeather },
   setup() {
     const handleClick = async () => {
       const options = {
@@ -40,4 +36,11 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#main {
+  background-image: url(https://cdn.qweather.com/img/plugin/190516/bg/h5/darkd.png);
+  background-repeat: no-repeat;
+  background-color: rgb(49, 58, 68);
+  background-position: center -23.4rem;
+}
+</style>
---
+++ src/views/home/components/Now-weather.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="main">
+    <div class="temperature">28°</div>
+    <div class="weather">
+      <img
+        src="	https://cdn.qweather.com/img/plugin/190516/icon/c/100d.png"
+        alt=""
+      />
+      <span>晴</span>
+    </div>
+    <div class="air-quality">
+      <span>良</span>
+      <span class="qualitynum">88</span>
+      <img
+        src="https://cdn.qweather.com/img/plugin/190516/icon/h5/aqi-right.png"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+.main {
+  padding-left: 0.8rem;
+  width: 100%;
+  height: 14rem;
+}
+.temperature {
+  margin-top: 6rem;
+  font-size: 4rem;
+  color: #fff;
+}
+.weather {
+  display: flex;
+  align-items: center;
+  font-size: 1.6rem;
+  color: #fff;
+}
+.weather img {
+  width: 2rem;
+  height: 2rem;
+}
+.air-quality {
+  margin-top: 0.6rem;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 4.6rem;
+  height: 1.6rem;
+  font-size: 0.8rem;
+  border-radius: 0.4rem;
+  background: rgba(121, 112, 112, 0.849);
+}
+.air-quality span {
+  margin-left: 0.5rem;
+}
+.qualitynum {
+  color: #a9a538;
+}
+.air-quality img {
+  width: 0.6rem;
+  height: 0.8rem;
+  margin-right: 0.5rem;
+}
+</style>
